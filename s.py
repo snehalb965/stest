15148a5ededa73b78f7df18965bab3d28ed12702
snehalb965 <snehalbnov19@gmail.com>
2026-02-19 18:17:45 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,17 @@
+# buggy_code.py
+
+def add_numbers(a, b)
+    # Missing colon and indentation error
+result = a + b
+return result
+
+def greet(name)
+    print("Hello, " + names)  # Variable name typo
+
+for i in range(5)
+print(i)  # Missing indentation
+
+my_list = [1, 2, 3]
+print(my_list[5])  # IndexError: out of range
+
+print("This line is fine"
